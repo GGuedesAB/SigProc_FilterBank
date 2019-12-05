c23e027b575f5fe501361f88b6d9098f3efa591f
GGuedesAB <gustavoguedesab@gmail.com>
2019-12-04 22:28:49 -0300

    Adding methods for calculating and printing FFT of audio

--- src/Audio.py
+++ src/Audio.py
@@ -1,5 +1,8 @@
 import os
+import matplotlib.pyplot as plt
+import numpy as np
 from scipy.io import wavfile
+from scipy import fftpack
 
 class Audio ():
     def __init__(self, sampling_rate=None, data=None):
@@ -12,6 +15,7 @@ class Audio ():
         else:
             raise FileNotFoundError
         self.sampling_rate, self.data = wavfile.read(self.__wav_path__)
+        print ('Loaded ' + str(os.path.basename(wav_path)) + ' with ' + str(len(self.data)) + ' data points and sampling rate of ' + str(self.sampling_rate) + 'Hz.')
 
     def save_audio (self, filename):
         wavfile.write(filename, self.sampling_rate, self.data)
@@ -22,10 +26,30 @@ class Audio ():
     def get_sampling_rate(self):
         return self.sampling_rate
 
+    def calculate_fft(self):
+        self.w_axis = fftpack.fftfreq(len(self.data), (1/self.sampling_rate))
+        self.fft_of_data = fftpack.fft(self.data)
+        return (self.w_axis, self.fft_of_data)
+
+    def print_fft(self):
+        self.calculate_fft()
+        mag = np.abs(self.fft_of_data)
+        plt.bar(self.w_axis, mag)
+        print ('Saving mag bars')
+        plt.savefig('marg_bars.png')
+        print ('Finished saving mag bars.')
+        angle = np.angle(self.fft_of_data)
+        plt.plot(self.w_axis, angle)
+        print ('Saving angle plot.')
+        plt.savefig('angle.png')
+        print ('Finished saving angle plot.')
+
+
 if __name__ == "__main__":
     my_audio = Audio()
     my_audio.load_audio('../audio/Blind_intro.wav')
     print(my_audio.get_sampling_rate())
-    print(my_audio.get_data()) 
+    print(my_audio.get_data())
+    my_audio.print_fft()
 
         
